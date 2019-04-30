750228ba8eeffd647a4c8353c882174560c6bbe7
Gleason <bianliuzhu@gmail.com>
2019-04-30 14:57:11 +0800

    配置 webpack 公用插件

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -5,7 +5,12 @@
  * 生产环境(prod)和开发环境(dev)通用配置
  */
 const webpack = require('webpack');
+
+// vue 核心解析插件
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+// html 模板解析插件
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/main.js', //入口
   module: {
@@ -21,5 +26,9 @@ module.exports = {
     new webpack.HashedModuleIdsPlugin(),
     // 请确保引入这个插件来施展魔法
     new VueLoaderPlugin(),
+    // html 模板解析 插件
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../index.html'),
+    })
   ],
 };
